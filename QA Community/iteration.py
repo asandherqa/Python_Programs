dc70fd1afa50ac3ae2f2c21aa51dc5ab9091c601
Aaron Sandher <asandherqa@gmail.com>
2021-06-04 15:09:01 +0000

    Python Intro

---
+++ QA Community/iteration.py
@@ -0,0 +1,13 @@
+count = 0
+name = str(input("What is your name?"))
+
+while count < 5:
+    print(name, "is awesome!")
+    count += 1
+    name = str(input("What is your name?"))
+
+for i in range(5,11):
+    print(i)
+
+for i in range(10,21,2):
+    print(i)
